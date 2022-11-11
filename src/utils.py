9cfb8414c46ac341c854580f78a23a759aaa6090
AlexisComix <thecomixguy@hotmail.com>
2022-11-11 15:30:53 +0000

    Add utils.py

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+"""
+This file contains common functions and variables
+for the game, such as basic mathematical calculations
+as well as variables.
+"""
+
+COLOURS = (
+    BLACK := (0, 0, 0),
+    GRAY := (127, 127, 127),
+    WHITE := (255, 255, 255),
+    RED := (255, 0, 0),
+    GREEN := (0, 255, 0),
+    BLUE := (0, 0, 255),
+    YELLOW := (255, 255, 0),
+    CYAN := (0, 255, 255),
+    MAGENTA := (255, 0, 255),
+)
