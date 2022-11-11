0529a5c36b992f975866b406b20e334db0ba709e
AlexisComix <thecomixguy@hotmail.com>
2022-11-11 13:38:36 +0000

    Add pygame boilerplate

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import os, sys
+import pygame
+
+class Game:
+    """
+    Main Game class for game logic and playing
+    """
+    def __init__(self):
+        """
+        Initialise instance of main game class
+        """
+        # Window size of 480p
+        self.WIN_WIDTH = 854
+        self.WIN_HEIGHT = 480
+        self.DIMENSIONS = (self.WIN_WIDTH, self.WIN_HEIGHT)
+        pygame.init()                   
+        self.WIN = pygame.display.set_mode(self.DIMENSIONS, vsync=1)
+
+        self.running = True
+        self.main()
+
+    def main(self):
+        clock = pygame.time.Clock()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False  
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
+            clock.tick(60)
+
+
+if __name__ == "__main__":
+    Game()
