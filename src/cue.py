6ba5f5ddf549fad9b10a6ef1bcef38faa2198376
AlexisComix <thecomixguy@hotmail.com>
2022-11-14 10:05:30 +0000

    (WIP) Start work on shooting ball

--- src/cue.py
+++ src/cue.py
@@ -2,9 +2,9 @@ import math
 
 import pygame
 
-import utils
+from utils import *
+import time
 from balls import Ball
-
 
 def blitRotateCenter(surf, image, topleft, angle):
 
@@ -18,15 +18,20 @@ def blitRotateCenter(surf, image, topleft, angle):
     surf.blit(rotated_image, new_rect)
 
 class Cue:
-    def __init__(self, x: float, y: float, cue_ball: Ball):
+    def __init__(self, x: float, y: float, cue_ball: Ball, screen: pygame.display):
         self.size = (250, 250)
         self.image = pygame.image.load("..\\assets\\cue-centred.png")
         self.image = pygame.transform.scale(self.image, self.size)
         self.angle = 0#deg
+        self.focusangle = 0#deg
         self.x = x
         self.y = y
         self.rect = pygame.rect.Rect(self.x, self.y, self.size[0], self.size[1])
         self.cue_ball = cue_ball
+        self.aoff = 45
+        self.power = 1
+        self.screen = screen
+        self.hide = False
 
     def focus(self, screen: pygame.display):
         # Set tip to centre of the ball focused
@@ -36,17 +41,43 @@ class Cue:
         (mousex, mousey) = pygame.mouse.get_pos()
 
         # Get angle between mouse and pivot point (top left)
-        dx = self.cue_ball.centrex - mousex
-        dy = self.cue_ball.centrey - mousey
-        self.angle = math.degrees(math.atan2(dx, dy)) - 45
+        self.dx = self.cue_ball.centrex - mousex
+        self.dy = self.cue_ball.centrey - mousey
+        self.angle = math.degrees(-math.atan2(self.dy, self.dx))
+        self.focusangle = self.angle + self.aoff
 
-    def draw(self, screen: pygame.display):
-        self.rect.center = (self.cue_ball.centrex, self.cue_ball.centrey)
-        blitRotateCenter(
-            screen, 
-            self.image, 
-            (
-                self.cue_ball.centrex - (self.size[0] / 2),
-                self.cue_ball.centrey - (self.size[1] / 2)
-            ), 
-            self.angle)
+    def draw(self):
+        if not self.hide:
+            self.rect.center = (self.cue_ball.centrex, self.cue_ball.centrey)
+            blitRotateCenter(
+                self.screen, 
+                self.image, 
+                (
+                    self.cue_ball.centrex - (self.size[0] / 2),
+                    self.cue_ball.centrey - (self.size[1] / 2)
+                ), 
+                self.focusangle)
+
+    # def shot_animation(self):
+    #     xmult = 1 if self.x <= self.cue_ball.centrex else -1
+    #     ymult = 1 if self.y <= self.cue_ball.centrey else -1
+    #     gradient = self.dy // self.dx
+    #     distance = 10
+    #     # Move back slowly along line
+    #     for _ in range(distance):
+    #         self.rect.center = (
+    #             self.cue_ball.centrex + (1*xmult),
+    #             self.cue_ball.centrey + (gradient*ymult)
+    #         )
+    #         blitRotateCenter(
+    #             self.screen,
+    #             self.image,
+    #             (
+    #                 self.rect.center[0] - (self.size[0]/2),
+    #                 self.rect.center[1] - (self.size[1]/2)
+    #             ),
+    #             self.focusangle
+    #         )
+    #         time.sleep(0.05)
+    #     # Move forward quickly along line
+    #     return
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os, sys          # Generic standard libraries
 
 import pygame           # Pygame for graphics
 
-import utils            # Utils module with common variables and functions
+from utils import *     # Utils module with common variables and functions
 from balls import Ball  # Ball class
 from cue import Cue     # Cue class
 
@@ -16,8 +16,8 @@ class Game:
         Initialise instance of main game class
         """
         # Window size of 480p
-        self.WIN_WIDTH = 854
-        self.WIN_HEIGHT = 480
+        self.WIN_WIDTH = WIDTH
+        self.WIN_HEIGHT = HEIGHT
         self.DIMENSIONS = (self.WIN_WIDTH, self.WIN_HEIGHT)
         pygame.init()                   
         self.WIN = pygame.display.set_mode(self.DIMENSIONS, vsync=1)
@@ -25,7 +25,7 @@ class Game:
         self.testball = Ball(self.WIN_WIDTH/2, self.WIN_HEIGHT/2, 8)
         self.testball.colour = (255, 255, 255)
         
-        self.cue = Cue(0, 0, self.testball)
+        self.cue = Cue(0, 0, self.testball, self.WIN)
 
         self.running = True
         self.main()
@@ -39,13 +39,17 @@ class Game:
                     self.running = False  
                     pygame.quit()
                     sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        print("shot")
+                        #self.cue.shot_animation()
 
             self.cue.focus(self.WIN)
 
             self.WIN.fill((0, 0, 0))
 
             self.testball.draw(self.WIN)
-            self.cue.draw(self.WIN)
+            self.cue.draw()
 
             pygame.display.flip()
             clock.tick(60)
